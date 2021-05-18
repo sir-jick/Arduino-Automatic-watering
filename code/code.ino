a4eaaa086a52d53f89efa7500c2ddf35856086c7
Sir_Jick <safaiypourabolfazl@gmail.com>
2021-05-18 17:54:42 +0430

    some cleaning and added LCD TODO

--- code/code.ino
+++ code/code.ino
@@ -1,31 +1,36 @@
-void setup(){
-
-Serial.begin(9600);
-pinMode(13, OUTPUT);
-}
-
-void loop(){
-digitalWrite(13, LOW);
 int soil_moisture=analogRead(A0);  // read from analog pin A0
+int water_pomp_relay = 13;
 
-Serial.print("analog value: ");
-Serial.println(soil_moisture);
-
-if(soil_moisture<30) {
-
-Serial.println("Dry soil");
-
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(water_pomp_relay , OUTPUT);
 }
 
-if((soil_moisture>300)&&(soil_moisture<700)) {
+void loop()
+{
+  digitalWrite(water_pomp_relay , LOW);
+  //TODO : write to LCD if Supposed to be
+  Serial.print("analog value: ");
+  Serial.println(soil_moisture);
 
-Serial.println("Humid soil");
-
-}
-
-if((soil_moisture>700)&&(soil_moisture<950)){
-  Serial.println("water");
-  digitalWrite(13, HIGH);
+  if(soil_moisture<30)
+  {
+    Serial.println("Dry soil");
+    //TODO : write to LCD if Supposed to be
   }
-    delay(500);
+
+  if((soil_moisture>300)&&(soil_moisture<700))
+  {
+    Serial.println("Humid soil");
+    //TODO : write to LCD if Supposed to be
+  }
+
+  if((soil_moisture>700)&&(soil_moisture<950))
+  {
+    //TODO : write to LCD if Supposed to be
+    Serial.println("water");
+    digitalWrite(water_pomp_relay , HIGH);
+  }
+  delay(500);
 }
