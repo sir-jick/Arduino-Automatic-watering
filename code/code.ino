52deb280d2a0e5073e14ae13fb617f65565dddb3
Sir_Jick <safaiypourabolfazl@gmail.com>
2021-05-22 00:18:52 +0430

    init branch and make TODOs for sms

--- code/code.ino
+++ code/code.ino
@@ -11,14 +11,14 @@ void loop()
 {
   int soil_moisture=analogRead(A0);  // read from analog pin A0
   digitalWrite(water_pomp_relay , LOW);
-  //TODO : write to LCD if Supposed to be
+  //TODO : send SMS if Supposed to be
   Serial.print("analog value: ");
   Serial.println(soil_moisture);
 
   if(soil_moisture<30)
   {
     Serial.println("Dry soil");
-    //TODO : write to LCD if Supposed to be
+    //TODO : send SMS if Supposed to be
     digitalWrite(water_pomp_relay,1);
     delay(3000);
   }
@@ -26,13 +26,13 @@ void loop()
   if((soil_moisture>300)&&(soil_moisture<700))
   {
     Serial.println("Humid soil");
-    //TODO : write to LCD if Supposed to be
+    //TODO : send SMS if Supposed to be
     digitalWrite(water_pomp_relay,0);
   }
 
   if((soil_moisture>700)&&(soil_moisture<950))
   {
-    //TODO : write to LCD if Supposed to be
+    //TODO : send SMS if Supposed to be
     Serial.println("watering");
     digitalWrite(water_pomp_relay , HIGH);
   }
