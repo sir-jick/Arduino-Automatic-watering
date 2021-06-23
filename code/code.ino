7ae51c83bbf2c98f2a5d3e5c8de9f84190aced4d
Sir_Jick <safaiypourabolfazl@gmail.com>
2021-06-23 09:40:42 +0430

    ready to send sms , almost

--- code/code.ino
+++ code/code.ino
@@ -1,11 +1,6 @@
 #include <SoftwareSerial.h>
 
 #define water_pomp_relay 13
-#define led_auto_mode 5
-#define led_manual_mode 6
-
-#define sms true // TODO  : it is for testing verification do NOT forget to remove
-
 
 //Create software serial object to communicate with SIM800L
 SoftwareSerial mySerial(3, 2); //SIM800L Tx & Rx is connected to Arduino #3 & #2
@@ -19,23 +14,7 @@ void setup()
 
 void loop()
 {
-  if(sms) // TODO : replace corect receive sms code for manual watering
-  {
-    digitalWrite(led_auto_mode, LOW);
-    digitalWrite(led_manual_mode, HIGH);
-    manual_watering();
-  }
-  else
-  {
-    digitalWrite(led_auto_mode, HIGH);
-    digitalWrite(led_manual_mode, LOW);
-    automatic_watering();
-  }
-  delay(3600*1000); // repeat until every 1 hour
-}
-
-void automatic_watering()
-{
+  
   int soil_moisture=analogRead(A0);  // read from analog pin A0
   digitalWrite(water_pomp_relay , LOW);
   //TODO : send SMS if Supposed to be
@@ -46,29 +25,31 @@ void automatic_watering()
   {
     Serial.println("Dry soil");
     //TODO : send SMS if Supposed to be
-    digitalWrite(water_pomp_relay,1);
+    digitalWrite(water_pomp_relay,HIGH);
     delay(3000);
+    if(soil_moisture<30)
+    {
+      send_sms("the container is out of water, please check the water source"); // TODO : change this function with right thing
+      delay(10000);
+    }
   }
 
   if((soil_moisture>300)&&(soil_moisture<700))
   {
     Serial.println("Humid soil");
-    //TODO : send SMS if Supposed to be
-    digitalWrite(water_pomp_relay,0);
+    //TODO : send send_sms if Supposed to be
+    Serial.println("watering");
+    send_sms("the system is watering now"); // TODO : change this function with right thing
+    digitalWrite(water_pomp_relay , HIGH);
+    delay(1000);
   }
 
   if((soil_moisture>700)&&(soil_moisture<950))
   {
-    //TODO : send SMS if Supposed to be
-    Serial.println("watering");
-    digitalWrite(water_pomp_relay , HIGH);
-    delay(500);
+    //TODO : send send_sms if Supposed to be
+
+    digitalWrite(water_pomp_relay,0);
+    send_sms("no need to watering ,I am not thirsty") // TODO : change this function with right thing
   }
-}
-void manual_watering()
-{
-  Serial.println("manual watering");
-  digitalWrite(water_pomp_relay , HIGH);
-  delay(3000);
-  digitalWrite(water_pomp_relay ,LOW);
+  delay(3600*1000); // repeat until every 1 hour
 }
